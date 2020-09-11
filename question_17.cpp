250ff9518dcd2b18123f7ebd60d57a226bbb81f2
eleve <eleve@lyceeppr.fr>
2020-09-11 15:08:03 +0200

    question_17

--- question_17.cpp
+++ question_17.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	int nbTab = 1;
+	int moy = 0;
 	
 	cout << "entre le nombre de note total : ";
 	cin >> nbTab;
@@ -15,5 +16,12 @@ int main()
 	{
 		cout << "entree la note " << i << " : ";
 		cin >> note[i];
+		moy = moy + note[i];
+	}
+	moy = moy/nbTab;
+	for (int i = 0; i < nbTab; ++i)
+	{
+		if(note[i] > moy)
+			cout << " la note " << i << " est superieur a la moyenne " << endl;
 	}
 }
